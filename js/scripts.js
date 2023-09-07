102396e69210820a5294fa28e40904b97cbc07d9
msparks714 <ms11269@nyu.edu>
2023-09-07 10:16:06 +0200

    plzzoom

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibXNwYXJrczcxNCIsImEiOiJjazZsZjl0aXAwYmMzM21uM
 // we want to return to this point and zoom level after the user interacts
 // with the map, so store them in variables
 var initialCenterPoint = [-8.961439, 48.535493]; 
-var initialZoom = 10.67;
+var initialZoom = 22.4;
 
 // create an object to hold the initialization options for a mapboxGL map
 var initOptions = {
